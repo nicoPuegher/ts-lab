21a65b02262925e77507d8f36bcd27338ad126d8
Nicolas <nicopuegher@gmail.com>
2025-01-02 14:53:58 -0300

    Display the text input safely

--- to-do/src/app.ts
+++ to-do/src/app.ts
@@ -2,4 +2,10 @@ import { createTextInputComponent } from '@components/text-input.ts';
 
 import './styles/globals.css';
 
+const app = document.querySelector<HTMLDivElement>('#app');
+
 const textInputComponent = createTextInputComponent();
+
+if (app) {
+    app.appendChild(textInputComponent);
+}
