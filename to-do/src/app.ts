55eabb84e05d642a780c196e76e51c5b7c7a4e5d
Nicolas <nicopuegher@gmail.com>
2025-01-02 14:53:37 -0300

    Import and create a text input

--- to-do/src/app.ts
+++ to-do/src/app.ts
@@ -1,3 +1,5 @@
+import { createTextInputComponent } from '@components/text-input.ts';
+
 import './styles/globals.css';
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = ` <div>hello world</div> `;
+const textInputComponent = createTextInputComponent();
