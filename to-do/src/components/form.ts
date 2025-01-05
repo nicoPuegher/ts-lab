52fc5cd32e5587b2ec0aed5c46e48aa6bc940a09
Nicolas <nicopuegher@gmail.com>
2025-01-05 16:51:35 -0300

    Link event with handler

--- to-do/src/components/form.ts
+++ to-do/src/components/form.ts
@@ -4,6 +4,8 @@ import { createTextInputComponent } from './text-input.ts';
 export function createFormComponent(): HTMLFormElement {
     const form = document.createElement('form');
 
+    form.addEventListener('submit', handleSubmit);
+
     const textInputComponent = createTextInputComponent();
     const buttonComponent = createButtonComponent('Add');
 
