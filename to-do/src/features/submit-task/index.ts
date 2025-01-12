371eb3742a5da01118e9d4e8e535b2b25d83c41c
Nicolas <nicopuegher@gmail.com>
2025-01-12 18:17:54 -0300

    Create a submit task feature index file

---
+++ to-do/src/features/submit-task/index.ts
@@ -0,0 +1,6 @@
+import { createButtonComponent } from '@components/button.ts';
+import { createFormComponent } from '@components/form.ts';
+import { createTextFeedbackComponent } from '@components/text-feedback.ts';
+import { createTextInputComponent } from '@components/text-input.ts';
+
+export function SubmitTask() {}
