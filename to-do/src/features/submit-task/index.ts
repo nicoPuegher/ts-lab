6423ab75595f2207b9336c51ff4c859ddd49f74c
Nicolas <nicopuegher@gmail.com>
2025-01-16 20:41:52 -0300

    Add rest of logic to submit task feature

--- to-do/src/features/submit-task/index.ts
+++ to-do/src/features/submit-task/index.ts
@@ -3,9 +3,19 @@ import { createFormComponent } from '@components/form.ts';
 import { createTextFeedbackComponent } from '@components/text-feedback.ts';
 import { createTextInputComponent } from '@components/text-input.ts';
 
-export function SubmitTask() {
+import { handleInput } from './handlers/handle-input.ts';
+import { handleSubmit } from './handlers/handle-submit.ts';
+
+export function submitTaskFeature() {
     const form = createFormComponent();
     const input = createTextInputComponent();
     const button = createButtonComponent('Add');
     const feedback = createTextFeedbackComponent();
+
+    input.addEventListener('input', (event) => handleInput(event as InputEvent, feedback));
+    form.addEventListener('submit', (event) => handleSubmit(event, input, feedback));
+
+    form.append(input, button, feedback);
+
+    return form;
 }
