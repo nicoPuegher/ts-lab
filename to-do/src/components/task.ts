07ce9dcd2489478e4a46158735b080f8be326250
Nicolas <nicopuegher@gmail.com>
2025-01-21 16:31:22 -0300

    Create a div element and assign a class

--- to-do/src/components/task.ts
+++ to-do/src/components/task.ts
@@ -1,1 +1,7 @@
-export function createTaskComponent(id: string, description: string): HTMLDivElement {}
+export function createTaskComponent(id: string, description: string): HTMLDivElement {
+    const div = document.createElement('div');
+
+    div.className = 'task';
+
+    return div;
+}
