2d1f76732790a18017862d5c6eca0809c7aac37f
Nicolas <nicopuegher@gmail.com>
2025-01-23 16:38:46 -0300

    Create a checkbox and its attributes

--- to-do/src/components/task.ts
+++ to-do/src/components/task.ts
@@ -3,5 +3,12 @@ export function createTaskComponent(id: string, description: string): HTMLDivEle
 
     div.className = 'task';
 
+    const checkbox = document.createElement('input');
+
+    checkbox.id = id;
+    checkbox.type = 'checkbox';
+    checkbox.checked = false;
+    checkbox.setAttribute('aria-labelledby', `label-${id}`);
+
     return div;
 }
