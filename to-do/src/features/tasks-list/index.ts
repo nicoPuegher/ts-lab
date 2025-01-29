69da63ed8196345fa40cde3410f57694932f45b8
Nicolas <nicopuegher@gmail.com>
2025-01-29 16:26:11 -0300

    Append an existing task

--- to-do/src/features/tasks-list/index.ts
+++ to-do/src/features/tasks-list/index.ts
@@ -1,10 +1,23 @@
+import { stateManager } from '@state/index.ts';
+
+import { createTaskComponent } from '@components/task.ts';
+
 export function createTasksList(): HTMLUListElement {
     const ul: HTMLUListElement = document.createElement('ul');
 
     function appendTasks(): void {
+        const { todos } = stateManager.getState();
+
         ul.replaceChildren();
+
+        todos.forEach((todo) => {
+            const task = createTaskComponent(todo.id, todo.text);
+
+            ul.appendChild(task);
+        });
     }
 
+    stateManager.subscribe(appendTasks);
     appendTasks();
 
     return ul;
