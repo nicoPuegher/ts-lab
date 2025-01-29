19379070ee6a3803dff16732ec0d4a39fb341769
Nicolas <nicopuegher@gmail.com>
2025-01-29 16:25:26 -0300

    Create a helper fn to append tasks

--- to-do/src/features/tasks-list/index.ts
+++ to-do/src/features/tasks-list/index.ts
@@ -1,5 +1,11 @@
 export function createTasksList(): HTMLUListElement {
     const ul: HTMLUListElement = document.createElement('ul');
 
+    function appendTasks(): void {
+        ul.replaceChildren();
+    }
+
+    appendTasks();
+
     return ul;
 }
