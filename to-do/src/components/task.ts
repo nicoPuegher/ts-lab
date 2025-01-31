f8c4e73eebca3aa1c0ed90969ae0fc4ca259ea64
Nicolas <nicopuegher@gmail.com>
2025-01-31 14:49:38 -0300

    Remove spaces in task

--- to-do/src/components/task.ts
+++ to-do/src/components/task.ts
@@ -1,10 +1,8 @@
 export function createTaskComponent(id: string, description: string): HTMLDivElement {
     const div = document.createElement('div');
-
     div.classList.add('task', 'shared');
 
     const checkbox = document.createElement('input');
-
     checkbox.id = id;
     checkbox.type = 'checkbox';
     checkbox.checked = false;
@@ -18,7 +16,6 @@ export function createTaskComponent(id: string, description: string): HTMLDivEle
     });
 
     const paragraph = document.createElement('p');
-
     paragraph.id = `label-${id}`;
     paragraph.textContent = description;
 
