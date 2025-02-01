05941cb46b0eafa2cd2bc1863ab2f54840d6ecc6
Nicolas <nicopuegher@gmail.com>
2025-02-01 14:36:37 -0300

    Change div to li for the task component

--- to-do/src/components/task.ts
+++ to-do/src/components/task.ts
@@ -1,26 +1,26 @@
 import { createIcons, icons } from 'lucide';
 
-export function createTaskComponent(id: string, description: string): HTMLDivElement {
-    const div: HTMLDivElement = document.createElement('div');
-    div.classList.add('task', 'shared');
+export function createTaskComponent(id: string, description: string): HTMLLIElement {
+    const li: HTMLLIElement = document.createElement('li');
+    li.classList.add('task', 'shared');
 
     const checkbox: HTMLInputElement = document.createElement('input');
     checkbox.id = id;
     checkbox.type = 'checkbox';
     checkbox.checked = false;
     checkbox.setAttribute('aria-labelledby', `label-${id}`);
-    checkbox.addEventListener('change', (event: Event) => handleChange(div, event));
+    checkbox.addEventListener('change', (event: Event) => handleChange(li, event));
 
     const paragraph: HTMLParagraphElement = document.createElement('p');
     paragraph.id = `label-${id}`;
     paragraph.textContent = description;
 
-    div.append(checkbox, paragraph);
+    li.append(checkbox, paragraph);
 
-    return div;
+    return li;
 }
 
-function handleChange(task: HTMLDivElement, event: Event): void {
+function handleChange(task: HTMLLIElement, event: Event): void {
     const checkbox = event.target as HTMLInputElement;
 
     if (checkbox.checked) {
