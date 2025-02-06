23cd72ed040f64641801920fbae11d426874c80b
Nicolas <nicopuegher@gmail.com>
2025-02-06 16:10:29 -0300

    Import and use process todos helper fn

--- to-do/src/features/tasks-list/index.ts
+++ to-do/src/features/tasks-list/index.ts
@@ -1,10 +1,8 @@
 import { stateManager } from '@state/index.ts';
 import type { Todo } from '@state/types/index.ts';
 
+import { processTodos } from '@features/tasks-list/helpers/process-todos.ts';
 import { removeIds } from '@features/tasks-list/helpers/remove-ids.ts';
-import { setNewTodo } from '@features/tasks-list/helpers/setNewTodo.ts';
-import { updateExistingTodo } from '@features/tasks-list/helpers/update-existing-todo.ts';
-import { updateTodos } from '@features/tasks-list/helpers/update-todos.ts';
 
 export function createTasksList(): HTMLUListElement {
     const elementsMap = new Map<string, HTMLLIElement>();
@@ -17,18 +15,7 @@ export function createTasksList(): HTMLUListElement {
         const { todos: newTodos }: { todos: Todo[] } = stateManager.getState();
 
         removeIds({ elementsMap, currentTodos, newTodos });
-
-        newTodos.forEach((todo) => {
-            const existingLi: HTMLLIElement = elementsMap.get(todo.id);
-
-            if (existingLi) {
-                updateExistingTodo({ existingLi, currentTodos, todo });
-            } else {
-                setNewTodo({ elementsMap, todo });
-            }
-
-            updateTodos({ elementsMap, currentTodos, newTodos, ul });
-        });
+        processTodos({ elementsMap, currentTodos, newTodos, ul });
     }
 
     stateManager.subscribe(appendTasks);
