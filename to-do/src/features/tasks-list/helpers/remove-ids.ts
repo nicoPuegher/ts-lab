f222ca748e30dd0a362fc0296b00a49165030daa
Nicolas <nicopuegher@gmail.com>
2025-02-06 15:26:32 -0300

    Refactor remove ids fn to a separete file

---
+++ to-do/src/features/tasks-list/helpers/remove-ids.ts
@@ -0,0 +1,22 @@
+import type { Todo } from '@state/types/index.ts';
+
+type removeIdsProps = {
+    currentTodos: Todo[];
+    newTodos: Todo[];
+    elementsMap: Map<string, HTMLLIElement>;
+};
+
+export function removeIds(props: removeIdsProps): void {
+    const currentIds = new Set<string>(props.currentTodos.map((currentTodo) => currentTodo.id));
+    const newIds = new Set<string>(props.newTodos.map((newTodo) => newTodo.id));
+
+    const idsToRemove: string[] = Array.from(currentIds).filter((id) => !newIds.has(id));
+
+    idsToRemove.forEach((id) => {
+        const li: HTMLLIElement = props.elementsMap.get(id);
+        if (li) {
+            li.remove();
+            props.elementsMap.delete(id);
+        }
+    });
+}
