dbe3fee98594ebecaa7e6d9cd9d18aef31cc58ff
Nicolas <nicopuegher@gmail.com>
2025-02-12 17:53:10 -0300

    Create content inside calendar item component

--- to-do/src/components/calendar-item.ts
+++ to-do/src/components/calendar-item.ts
@@ -1,1 +1,14 @@
-export function createCalendarItemComponent(day: string, date: number): HTMLDivElement {}
+export function createCalendarItemComponent(day: string, date: number): HTMLDivElement {
+    const div = document.createElement('div');
+    div.classList.add('calendar-item');
+
+    const dayElement = document.createElement('span');
+    dayElement.textContent = day.toUpperCase();
+
+    const dateElement = document.createElement('span');
+    dateElement.textContent = date.toString();
+
+    div.append(dayElement, dateElement);
+
+    return div;
+}
