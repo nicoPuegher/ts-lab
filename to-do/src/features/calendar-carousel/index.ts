c716629560ed4de5143e63862ced67666b2441dc
Nicolas <nicopuegher@gmail.com>
2025-02-13 18:13:43 -0300

    Create a generate week helper function

--- to-do/src/features/calendar-carousel/index.ts
+++ to-do/src/features/calendar-carousel/index.ts
@@ -2,5 +2,17 @@ export function createCalendarCarousel(): HTMLDivElement {
     const container = document.createElement('div');
     container.classList.add('calendar-carousel');
 
+    function generateWeek() {
+        const weekDays = 7;
+
+        return Array.from({ length: weekDays }, (_, i) => {
+            const date = new Date();
+            const newDay = date.getDate() + i;
+            date.setDate(newDay);
+
+            return date;
+        });
+    }
+
     return container;
 }
