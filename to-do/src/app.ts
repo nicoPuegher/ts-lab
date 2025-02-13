b118d64547f3acac68d0550efc94ad30099eb12a
Nicolas <nicopuegher@gmail.com>
2025-02-13 18:17:22 -0300

    Import and append calendar carousel to the app

--- to-do/src/app.ts
+++ to-do/src/app.ts
@@ -1,3 +1,4 @@
+import { createCalendarCarousel } from '@features/calendar-carousel/index.ts';
 import { createTaskSubmission } from '@features/task-submission/index.ts';
 import { createTasksList } from '@features/tasks-list/index.ts';
 
@@ -5,5 +6,6 @@ import './styles/globals.css';
 
 const app = document.querySelector<HTMLDivElement>('#app');
 
+app.appendChild(createCalendarCarousel());
 app.appendChild(createTasksList());
 app.appendChild(createTaskSubmission());
