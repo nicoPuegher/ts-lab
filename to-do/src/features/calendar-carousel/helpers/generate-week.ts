d587fdb084632a66c003d8231b0b57e20f6a2bb1
Nicolas <nicopuegher@gmail.com>
2025-02-14 16:36:56 -0300

    Refactor and use the new generate week helper fn

--- to-do/src/features/calendar-carousel/helpers/generate-week.ts
+++ to-do/src/features/calendar-carousel/helpers/generate-week.ts
@@ -1,6 +1,6 @@
 const WEEK_DAYS = 7;
 
-export function generateWeek() {
+export function generateWeek(): Date[] {
     return Array.from({ length: WEEK_DAYS }, (_, i) => {
         const date = new Date();
         const newDay = date.getDate() + i;
--- to-do/src/features/calendar-carousel/index.ts
+++ to-do/src/features/calendar-carousel/index.ts
@@ -1,20 +1,10 @@
 import { createCalendarItemComponent } from '@components/calendar-item.ts';
 
+import { generateWeek } from '@features/calendar-carousel/helpers/generate-week.ts';
+
 export function createCalendarCarousel(): HTMLDivElement {
     const container = document.createElement('div');
     container.classList.add('calendar-carousel');
-
-    function generateWeek() {
-        const weekDays = 7;
-
-        return Array.from({ length: weekDays }, (_, i) => {
-            const date = new Date();
-            const newDay = date.getDate() + i;
-            date.setDate(newDay);
-
-            return date;
-        });
-    }
 
     const dates = generateWeek();
 
