c102437a15b3af6e9d66eadd524f76aba34f13a9
Nicolas <nicopuegher@gmail.com>
2025-02-20 17:52:50 -0300

    Update create calendar item component paras and give each child an id

--- to-do/src/components/calendar-item.ts
+++ to-do/src/components/calendar-item.ts
@@ -1,12 +1,13 @@
-export function createCalendarItemComponent(day: string, date: number): HTMLDivElement {
+export function createCalendarItemComponent(day: string, dateNumber: number, date: Date): HTMLDivElement {
     const div = document.createElement('div');
+    div.setAttribute('id', date.toLocaleDateString().toString());
     div.classList.add('calendar-item');
 
     const dayElement = document.createElement('span');
     dayElement.textContent = day.toUpperCase();
 
     const dateElement = document.createElement('span');
-    dateElement.textContent = date.toString();
+    dateElement.textContent = dateNumber.toString();
 
     div.append(dayElement, dateElement);
 
