0e7bdca83bcd6b69efe830780778b613a4f77316
Nicolas <nicopuegher@gmail.com>
2025-02-25 17:46:43 -0300

    Calculate left fade

--- to-do/src/helpers/add-carousel-gradient.ts
+++ to-do/src/helpers/add-carousel-gradient.ts
@@ -4,4 +4,7 @@ function updateScrollState() {
     const scrollLeft = carousel.scrollLeft;
     const maxScroll = carousel.scrollWidth - carousel.clientWidth;
     const fadeDistance = 30;
+
+    const leftFade = Math.min((scrollLeft / fadeDistance) * 10, 10);
+    carousel.style.setProperty('--left-fade', `${leftFade}%`);
 }
