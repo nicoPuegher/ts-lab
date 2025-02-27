4ab6c9be64b42a6a521e91e00db3b5fb8d7637e8
Nicolas <nicopuegher@gmail.com>
2025-02-27 10:30:57 -0300

    Remove css import from app.ts

--- to-do/src/app.ts
+++ to-do/src/app.ts
@@ -5,8 +5,6 @@ import { createCalendarCarousel } from '@features/calendar-carousel/index.ts';
 import { createTaskSubmission } from '@features/task-submission/index.ts';
 import { createTasksList } from '@features/tasks-list/index.ts';
 
-import './styles/globals.css';
-
 const app = document.querySelector<HTMLDivElement>('#app');
 
 app.appendChild(createCalendarCarousel());
