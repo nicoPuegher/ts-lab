428cc07700a8bf9ddea7b634b8996b4a49c3bc84
Nicolas <nicopuegher@gmail.com>
2025-03-13 12:09:27 -0300

    Pass the variant class to the button element

--- to-do/src/components/button.ts
+++ to-do/src/components/button.ts
@@ -1,6 +1,7 @@
 export function createButtonComponent(textContent: string, variant: string = 'primary'): HTMLButtonElement {
     const button: HTMLButtonElement = document.createElement('button');
     button.textContent = textContent;
+    button.classList.add(variant);
 
     return button;
 }
