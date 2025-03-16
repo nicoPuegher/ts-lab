da3c86796ee786c73359a1f7ab67d37719b181a6
Nicolas <nicopuegher@gmail.com>
2025-03-16 12:15:38 -0300

    Give div the .filter-container class

--- to-do/src/features/task-filter/index.ts
+++ to-do/src/features/task-filter/index.ts
@@ -1,5 +1,6 @@
 export function createTaskFilter(): HTMLDivElement {
     const container = document.createElement('div');
+    container.classList.add('filter-container');
 
     return container;
 }
