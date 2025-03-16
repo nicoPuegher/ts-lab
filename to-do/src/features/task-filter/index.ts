207cf0fc340e0c098e3d2d97375ad09859b93678
Nicolas <nicopuegher@gmail.com>
2025-03-16 12:16:40 -0300

    Create three buttons with filter variant

--- to-do/src/features/task-filter/index.ts
+++ to-do/src/features/task-filter/index.ts
@@ -2,5 +2,9 @@ export function createTaskFilter(): HTMLDivElement {
     const container = document.createElement('div');
     container.classList.add('filter-container');
 
+    const allButton = createButtonComponent('All', 'filter');
+    const activeButton = createButtonComponent('Active', 'filter');
+    const completedButton = createButtonComponent('Completed', 'filter');
+
     return container;
 }
