169958944338657b37390a7e8422af4704702476
Nicolas <nicopuegher@gmail.com>
2025-03-25 12:14:17 -0300

    Remove unused imports in tasks-list

--- to-do/src/features/tasks-list/index.ts
+++ to-do/src/features/tasks-list/index.ts
@@ -1,9 +1,9 @@
 import { stateManager } from '@state/index.ts';
 import type { Todo } from '@state/types/index.ts';
 
-import { processTodos } from '@features/tasks-list/helpers/process-todos.ts';
-import { removeIds } from '@features/tasks-list/helpers/remove-ids.ts';
-import { scrollToLast } from '@features/tasks-list/helpers/scroll-to-last.ts';
+import { createTaskComponent } from '@components/task';
+
+//import { scrollToLast } from '@features/tasks-list/helpers/scroll-to-last.ts';
 
 export function createTasksList(): HTMLUListElement {
     const ul = document.createElement('ul');
