1d8ff07ff351270d5da3296d0f67854ea40f76e0
Nicolas <nicopuegher@gmail.com>
2025-04-04 15:22:13 -0300

    Toggle removed class on icon container element

--- to-do/src/components/task.ts
+++ to-do/src/components/task.ts
@@ -20,6 +20,7 @@ export function createTaskComponent(todo: Todo): HTMLLIElement {
 
     const iconContainer: HTMLDivElement = document.createElement('div');
     iconContainer.addEventListener('click', () => handleDeleteTodo(todo.id));
+    iconContainer.classList.toggle('removed', todo.completed);
     const icon: HTMLElement = document.createElement('i');
     icon.classList.add('icon');
     icon.setAttribute('data-lucide', 'trash');
