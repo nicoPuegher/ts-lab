23e382f8b277aee004195de7bc75cb7417d52775
Nicolas <nicopuegher@gmail.com>
2025-04-08 15:28:20 -0300

    Create buttons for the tasks-filter feature

--- to-do/src/features/tasks-filter/index.ts
+++ to-do/src/features/tasks-filter/index.ts
@@ -1,6 +1,12 @@
+import { createButtonComponent } from '@components/button.ts';
+
 export function createTasksFilter(): HTMLDivElement {
     const container = document.createElement('div');
     container.classList.add('filters-container');
+
+    const allButton = createButtonComponent('All', 'filter');
+    const activeButton = createButtonComponent('Active', 'filter');
+    const completedButton = createButtonComponent('Completed', 'filter');
 
     return container;
 }
