211d2a4faf81ff3ccffd054cb1966f4863864825
Nicolas <nicopuegher@gmail.com>
2025-04-08 15:28:47 -0300

    Add event listeners to the buttons in tasks-filter feature

--- to-do/src/features/tasks-filter/index.ts
+++ to-do/src/features/tasks-filter/index.ts
@@ -1,3 +1,5 @@
+import { stateManager } from '@state/index.ts';
+
 import { createButtonComponent } from '@components/button.ts';
 
 export function createTasksFilter(): HTMLDivElement {
@@ -7,6 +9,10 @@ export function createTasksFilter(): HTMLDivElement {
     const allButton = createButtonComponent('All', 'filter');
     const activeButton = createButtonComponent('Active', 'filter');
     const completedButton = createButtonComponent('Completed', 'filter');
+
+    allButton.addEventListener('click', () => stateManager.setFilter('all'));
+    activeButton.addEventListener('click', () => stateManager.setFilter('active'));
+    completedButton.addEventListener('click', () => stateManager.setFilter('completed'));
 
     return container;
 }
