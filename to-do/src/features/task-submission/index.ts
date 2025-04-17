c5c9d12747711e61af31ae398da4791dd0f56fb3
Nicolas <nicopuegher@gmail.com>
2025-04-17 15:53:19 -0300

    Use the new label component in task-submission feature

--- to-do/src/features/task-submission/index.ts
+++ to-do/src/features/task-submission/index.ts
@@ -1,5 +1,6 @@
 import { createButtonComponent } from '@components/button.ts';
 import { createFormComponent } from '@components/form.ts';
+import { createLabelComponent } from '@components/label.ts';
 import { createTextFeedbackComponent } from '@components/text-feedback.ts';
 import { createTextInputComponent } from '@components/text-input.ts';
 
@@ -9,13 +10,14 @@ import { handleSubmit } from './handlers/handle-submit.ts';
 export function createTaskSubmission(): HTMLFormElement {
     const form: HTMLFormElement = createFormComponent();
     const input: HTMLInputElement = createTextInputComponent();
+    const label: HTMLLabelElement = createLabelComponent(input.id, 'Todo item');
     const button: HTMLButtonElement = createButtonComponent('Add');
     const feedback: HTMLParagraphElement = createTextFeedbackComponent();
 
     input.addEventListener('input', (event: InputEvent) => handleInput(event as InputEvent, feedback));
     form.addEventListener('submit', (event: SubmitEvent) => handleSubmit(event as SubmitEvent, form, input, feedback));
 
-    form.append(input, button, feedback);
+    form.append(label, input, button, feedback);
 
     return form;
 }
