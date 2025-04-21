bded61247555f93f65fd2ee659fb83b4c67f6b0f
Nicolas <nicopuegher@gmail.com>
2025-04-21 16:27:39 -0300

    Create a search icon

--- to-do/src/features/tasks-filter-search/index.ts
+++ to-do/src/features/tasks-filter-search/index.ts
@@ -2,5 +2,9 @@ export function createTasksFilterSearch(): HTMLDivElement {
     const searchContainer = document.createElement('div');
     searchContainer.classList.add('search-container');
 
+    const searchIcon = document.createElement('i');
+    searchIcon.setAttribute('data-lucide', 'search');
+    searchIcon.classList.add('search-icon');
+
     return searchContainer;
 }
