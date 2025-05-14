3cf346832fe2642acd3a9c2e9e5a4a19c1502e3b
Nicolas <nicopuegher@gmail.com>
2025-05-14 15:03:50 -0300

    Remove #app id and use main element instead

--- to-do/src/app.ts
+++ to-do/src/app.ts
@@ -8,7 +8,7 @@ import { createTasksList } from '@features/tasks-list/index.ts';
 
 import './styles/globals.css';
 
-const app = document.querySelector<HTMLDivElement>('#app');
+const app = document.querySelector('main');
 
 app.appendChild(createCalendarCarousel());
 scrollToToday();
