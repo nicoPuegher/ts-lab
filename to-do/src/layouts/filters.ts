bc3f5d2206933f091365f8f78118ff6e12261a53
Nicolas <nicopuegher@gmail.com>
2025-05-18 11:27:14 -0300

    Rename filter layout container

--- to-do/src/layouts/filters.ts
+++ to-do/src/layouts/filters.ts
@@ -2,8 +2,8 @@ import { createTasksFilterButtons } from '@features/tasks-filter-buttons/index.t
 import { createTasksFilterSearch } from '@features/tasks-filter-search/index.ts';
 
 export function createFilterLayout(): HTMLDivElement {
-    const todoFiltersContainer = document.createElement('div');
-    todoFiltersContainer.classList.add('todo-filters-container');
+    const container = document.createElement('div');
+    container.classList.add('todo-filters-container');
 
     const filterButtons = createTasksFilterButtons();
     const filterSearch = createTasksFilterSearch();
