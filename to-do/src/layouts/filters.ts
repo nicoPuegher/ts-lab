beef44b88d6c45d4c70bfdabbe52b482a5319f45
Nicolas <nicopuegher@gmail.com>
2025-05-19 11:47:36 -0300

    Rename status-filter references

--- to-do/src/layouts/filters.ts
+++ to-do/src/layouts/filters.ts
@@ -1,14 +1,14 @@
-import { createTasksFilterButtons } from '@features/tasks-filter-buttons/index.ts';
+import { createStatusFilter } from '@features/status-filter/index.ts';
 import { createTasksFilterSearch } from '@features/tasks-filter-search/index.ts';
 
 export function createFilterLayout(): HTMLDivElement {
     const container = document.createElement('div');
     container.classList.add('filter-layout');
 
-    const filterButtons = createTasksFilterButtons();
+    const statusFilter = createStatusFilter();
     const filterSearch = createTasksFilterSearch();
 
-    container.append(filterButtons, filterSearch);
+    container.append(statusFilter, filterSearch);
 
     return container;
 }
