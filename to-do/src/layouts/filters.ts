6c199c0b265ba51276095964e2236e5f6cfa52d4
Nicolas <nicopuegher@gmail.com>
2025-05-20 12:20:49 -0300

    Update search-filter references

--- to-do/src/layouts/filters.ts
+++ to-do/src/layouts/filters.ts
@@ -1,14 +1,14 @@
+import { createSearchFilter } from '@features/search-filter/index.ts';
 import { createStatusFilter } from '@features/status-filter/index.ts';
-import { createTasksFilterSearch } from '@features/tasks-filter-search/index.ts';
 
 export function createFilterLayout(): HTMLDivElement {
     const container = document.createElement('div');
     container.classList.add('filter-layout');
 
     const statusFilter = createStatusFilter();
-    const filterSearch = createTasksFilterSearch();
+    const searchFilter = createSearchFilter();
 
-    container.append(statusFilter, filterSearch);
+    container.append(statusFilter, searchFilter);
 
     return container;
 }
