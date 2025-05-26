6a6f35977a565605cd4022ed009aea0a02ba7c1d
Nicolas <nicopuegher@gmail.com>
2025-05-26 16:50:23 -0300

    Update references to todo-submission

--- to-do/src/app.ts
+++ to-do/src/app.ts
@@ -3,15 +3,15 @@ import { scrollToToday } from '@/helpers/scroll-to-today.ts';
 import { createFilterLayout } from '@/layouts/filters.ts';
 
 import { createDatePicker } from '@features/date-picker/index.ts';
-import { createTaskSubmission } from '@features/task-submission/index.ts';
 import { createTodoList } from '@features/todo-list/index.ts';
+import { createTodoSubmission } from '@features/todo-submission/index.ts';
 
 import './styles/globals.css';
 
 const rootElement = document.querySelector('#root');
 if (!rootElement) throw new Error('The #root element does not exist.');
 
-rootElement.append(createDatePicker(), createFilterLayout(), createTodoList(), createTaskSubmission());
+rootElement.append(createDatePicker(), createFilterLayout(), createTodoList(), createTodoSubmission());
 
 scrollToToday();
 addCarouselGradient();
