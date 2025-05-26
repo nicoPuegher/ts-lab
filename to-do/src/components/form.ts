58ced0000a858729024c0ce668e74c1f4ad0818e
Nicolas <nicopuegher@gmail.com>
2025-05-26 16:35:11 -0300

    Remove unnecessary type in form component

--- to-do/src/components/form.ts
+++ to-do/src/components/form.ts
@@ -1,5 +1,5 @@
 export function createFormComponent(): HTMLFormElement {
-    const form: HTMLFormElement = document.createElement('form');
+    const form = document.createElement('form');
 
     return form;
 }
