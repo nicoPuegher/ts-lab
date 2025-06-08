89877e2f709694d4afb7bd723494020bcec14e18
Nicolas <nicopuegher@gmail.com>
2025-06-08 12:20:43 -0300

    Import format-date utility fn in date component

--- to-do/src/components/date.ts
+++ to-do/src/components/date.ts
@@ -1,3 +1,7 @@
+import { formatDate } from '@/utils/format-date.ts';
+
+import { stateManager } from '@state/index.ts';
+
 export function createDateComponent(weekday: string, dayOfMonth: number, date: Date) {
     const container = document.createElement('div');
     container.id = date.toLocaleDateString().toString();
