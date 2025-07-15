0057addc3d013d305c029ecab62582dbf1b68b44
Nicolas <nicopuegher@gmail.com>
2025-07-15 14:04:18 -0300

    Replace div for button in scrollToToday helper fn to fix it

--- to-do/src/helpers/scroll-to-today.ts
+++ to-do/src/helpers/scroll-to-today.ts
@@ -3,7 +3,7 @@ import { formatDate } from '@/utils/format-date.ts';
 export function scrollToToday() {
     const todayDateContainer = document.getElementById(formatDate(new Date()));
 
-    if (!(todayDateContainer instanceof HTMLDivElement)) return;
+    if (!(todayDateContainer instanceof HTMLButtonElement)) return;
 
     todayDateContainer.scrollIntoView({ behavior: 'smooth', block: 'center', inline: 'start' });
 }
