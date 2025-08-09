b09037fdae7a990f8c46681ee1090b9541afbf81
Nicolas <nicopuegher@gmail.com>
2025-08-09 12:30:12 -0300

    Give button component a tabindex of -1

--- to-do/src/components/button.ts
+++ to-do/src/components/button.ts
@@ -1,6 +1,7 @@
 export function createButtonComponent(textContent: string, variant: string = 'primary') {
     const button = document.createElement('button');
     button.textContent = textContent;
+    button.setAttribute('tabindex', '-1');
     button.classList.add(variant);
 
     return button;
