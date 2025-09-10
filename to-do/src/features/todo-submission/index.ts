26ec3991b59aa506eaffafda1467f5e66b8597ef
Nicolas <nicopuegher@gmail.com>
2025-09-10 17:07:38 -0300

    Set error element 'aria-live' attribute

--- to-do/src/features/todo-submission/index.ts
+++ to-do/src/features/todo-submission/index.ts
@@ -28,6 +28,7 @@ export function createTodoSubmission() {
 
     const errorElement = createErrorComponent();
     errorElement.setAttribute('role', 'alert');
+    errorElement.setAttribute('aria-live', 'polite');
 
     input.addEventListener('input', () => handleClearValidationFeedback(errorElement));
     form.addEventListener('submit', (event) => handleTodoSubmission(event, form, input, errorElement));
